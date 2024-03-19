e017d7434b041e09dd860d121838e56b6c694775
Akshay <varma.akshay121@gmail.com>
2024-03-19 13:04:45 +0530

    what is currying

--- closures/currying-dom.js
+++ closures/currying-dom.js
@@ -1,3 +1,7 @@
+// Currying is a function that takes one argument at a time and returns a new function expecting the next argument. 
+// It is a conversion of functions from callable as f(a,b,c)into callable as f(a)(b)(c).
+
+
 // One of the usecase of currying where we are manipulating the dom
 
 const updatedElement = id => content => document.querySelector(`#${id}`).textContent = content;
